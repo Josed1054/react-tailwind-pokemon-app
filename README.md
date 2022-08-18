8c5f4147199443ce277568d0a4118114a3b3d139
Josed1054 <61340308+Josed1054@users.noreply.github.com>
2022-08-18 13:38:49 -0500

    Added Pokemons page and Updated Readme

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-Hi!!
+Poke APP
+
+This Web App consumes the [Poke API](https://pokeapi.co/) for displaying information about Pokemons.
+-On the Home page there are displayed 100 random Pokemons, and you can filter them.
+-On the Pokemon page there is information for each pokemon, where you can see: data, 25 random locations and make a comparison between 2 more Pokemons.
+-On the Pokemons page there are displayed all the Pokemons sorted ascending by id and displayed in groups of 20.
+-On the Search page there are displayed the results of matching Pokemons.
+
+Technologies: -[React](https://create-react-app.dev/) -[Tailwind CSS](https://tailwindcss.com/) -[React Router](https://reactrouter.com/) -[Auto-Animate](https://auto-animate.formkit.com/) -[React-Icons](https://react-icons.github.io/react-icons)
+
+Used for:
+-React: Create the app.
+-Tailwind CSS: Style the app and make it responsive.
+-React Router: Make the routes for the app and navigation.
+-Auto-Animate: Easily animate the app.
+-React-Icons: Add and use Icons to the app.
--- src/pages/pokemons/index.tsx
+++ src/pages/pokemons/index.tsx
@@ -1,5 +1,95 @@
-import React from "react";
+import React, { SyntheticEvent, useEffect, useState } from "react";
+import { useAutoAnimate } from "@formkit/auto-animate/react";
+import useFetch from "../../custom-hooks/fetch";
+import { DisplayPokemonMin } from "../../components/display-pokemons";
 
 export function Pokemons() {
-  return <p>Pokemons</p>;
+  // handle animation
+  const [animation] = useAutoAnimate();
+
+  // handle states
+  const [page, setPage] = useState(1);
+  const [offset, setOffset] = useState(0);
+  const [records, setRecords] = useState(20);
+
+  // fetch by page, and update record count
+  const {
+    data: pokemons,
+    loading,
+    error,
+  } = useFetch(`https://pokeapi.co/api/v2/pokemon/?offset=${offset}&limit=20`);
+
+  useEffect(() => {
+    if (!loading) {
+      setRecords((page - 1) * 20 + pokemons.results.length);
+    }
+  }, [pokemons]);
+
+  if (loading) return <h1>Loading...</h1>;
+  if (error) {
+    console.log(new Error(error));
+    return <h2>Error</h2>;
+  }
+
+  // handle pagination
+  function changePage(event: SyntheticEvent) {
+    const { name } = event.target as HTMLButtonElement;
+
+    if (name === "next") {
+      setPage((prevPage) => prevPage + 1);
+      setOffset((prevData) => prevData + pokemons.results.length);
+    } else if (name === "prev") {
+      setPage((prevPage) => prevPage - 1);
+      setOffset((prevData) => prevData - pokemons.results.length);
+    }
+  }
+
+  function displayPokemons() {
+    return pokemons.results.map((pokemon: any, index: number) => {
+      return <DisplayPokemonMin key={index} pokemonURL={pokemon.url} />;
+    });
+  }
+
+  return (
+    <div
+      ref={animation as React.RefObject<HTMLDivElement>}
+      className="flex flex-wrap mx-auto my-4 md:w-3/4 md:max-width-3/4 md:justify-between md:gap-4 max-w-7xl"
+    >
+      <div className="flex-[100%] flex flex-wrap justify-around">
+        <button
+          type="button"
+          className={`border-solid border-2 border-red-200 rounded-lg w-[15vw] max-w-[76px] md:w-[7vw] max-h-[4vh] ${
+            pokemons.previous === "" ||
+            pokemons.previous === null ||
+            pokemons.previous === undefined
+              ? "invisible"
+              : ""
+          }`}
+          name="prev"
+          onClick={changePage}
+        >
+          Back
+        </button>
+        <p className="self-center w-1/2 text-center text-x md:text-xl">{`Pokemons ${
+          records - pokemons.results.length + 1
+        } - ${records}`}</p>
+        <button
+          type="button"
+          className={`border-solid border-2 border-red-200 rounded-lg w-[15vw] max-w-[76px] md:w-[7vw] max-h-[4vh] ${
+            pokemons.next === "" ||
+            pokemons.next === null ||
+            pokemons.next === undefined
+              ? "invisible"
+              : ""
+          }`}
+          name="next"
+          onClick={changePage}
+        >
+          Next
+        </button>
+      </div>
+
+      {displayPokemons()}
+    </div>
+  );
 }
